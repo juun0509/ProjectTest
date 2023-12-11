11c4a85c8845a3a7b04ff981f527a89a8a86b303
hong-gp <127091213+hong-gp@users.noreply.github.com>
2023-12-11 16:39:22 +0900

    1211

---
+++ dao/CommunityDaoImpl.java
@@ -0,0 +1,55 @@
+package com.green.tonicbank.dao;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.green.tonicbank.model.Community;
+import com.green.tonicbank.model.SearchCondition;
+
+@Repository
+public class CommunityDaoImpl implements CommunityDao {
+
+	@Autowired
+	SqlSession session;
+	
+	String namespace = "com.green.tonicbank.Community.";
+	
+	@Override
+	public int deleteAll() throws Exception  {
+		return session.delete(namespace + "deleteAll");
+	}
+	
+	@Override
+	public int delete(Integer communityId, String userId) throws Exception  {
+		Map map = new HashMap();
+		map.put("communityId", communityId);
+		map.put("userId", userId);
+		return session.delete(namespace + "delete", map);
+	}
+	
+	@Override
+	public int insert(Community community) throws Exception {
+		return session.insert(namespace + "insert", community);
+	}
+	
+	@Override
+	public List<Community> selectAll(SearchCondition sc) throws Exception {
+		
+		return session.selectList(namespace + "selectAll", sc);
+	}
+
+	@Override
+	public int count(SearchCondition sc) throws Exception {
+		return session.selectOne(namespace + "count", sc);
+	}
+
+	@Override
+	public Community select(Integer communityId) throws Exception {
+		return session.selectOne(namespace + "select", communityId);
+	}
+}
