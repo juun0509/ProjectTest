52c1a8dbd4e6a41d0aeed59b7b6e4e870e253629
juun0509 <142364303+juun0509@users.noreply.github.com>
2023-12-19 00:03:22 +0900

    상품찾기

--- src/main/java/com/green/tonicbank/controller/ProductController.java
+++ src/main/java/com/green/tonicbank/controller/ProductController.java
@@ -5,15 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.green.tonicbank.dao.ProductDao;
 import com.green.tonicbank.model.Custom;
 import com.green.tonicbank.model.Product;
-import com.green.tonicbank.model.SearchCondition;
 import com.green.tonicbank.service.ProductService;
 
 @Controller
@@ -22,16 +22,26 @@ public class ProductController {
 
 	@Autowired
 	ProductService ps;
+
+	@Autowired
+	ProductDao pdao;
 	
 	@GetMapping
 	public String getProduct() throws Exception{
+		return "findProduct";
+	}
+	@GetMapping("/custom")
+	public String getCustom() throws Exception{
 		return "product";
 	}
-	
 	@PostMapping("/custom")
 	public ResponseEntity<List<List<Product>>> getCustom(@RequestBody Custom request) throws Exception {
 	    String tagsStr = String.join(",", request.getTags());
 	    return ResponseEntity.ok(ps.getCustom(tagsStr, Integer.parseInt(request.getPriceMin()), Integer.parseInt(request.getPriceMax())));
 	}
-	
+	@PostMapping("find")
+	@ResponseBody
+	public List<Product> findProduct(@RequestBody Product product)throws Exception {
+	    return pdao.find(product);
+	}
 }
